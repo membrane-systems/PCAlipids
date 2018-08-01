51410e229b483c215c2c22b07be86df11475bbb1
Khalid <khalid.mustafin@gmail.com>
2018-08-01 22:19:53 +0300

    analysis of 2 trajectories

--- scr/eigenvecdot.py
+++ scr/eigenvecdot.py
@@ -37,15 +37,37 @@ def main(file1, file2):
 		for j in range(100):
 			matrix[i][j] = abs(np.dot(eigenvecs1[i], eigenvecs2[j]))
 
-	plt.imshow(matrix, interpolation='nearest', origin='lower', cmap='gray_r')
+
+	fig = plt.figure()
+	ax1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+	ax2 = fig.add_axes([0.22, 0.64, 0.24, 0.24])
+	ax1.imshow(matrix, interpolation='nearest', origin='lower', cmap='gray_r')
+	plt.sca(ax1)
+	ax1.set_xticks([0,9,19,29,39,49,59,69,79,89,99]);
+	ax1.set_yticks([0,9,19,29,39,49,59,69,79,89,99]);
+	ax1.set_xticklabels([1,10,20,30,40,50,60,70,80,90,100]);
+	ax1.set_yticklabels([1,10,20,30,40,50,60,70,80,90,100]);
+	matrix = [[0.0] * 10 for i in range(10)]
+
+	for i in range(10):
+		for j in range(10):
+			matrix[i][j] = abs(np.dot(eigenvecs1[i], eigenvecs2[j]))
+	ax2.imshow(matrix, interpolation='nearest', origin='lower', cmap='gray_r')
+	plt.sca(ax2)
+	ax2.set_xticks([0,4,9]);
+	ax2.set_yticks([0,4,9]);
+	ax2.set_xticklabels([1,5,10]);
+	ax2.set_yticklabels([1,5,10]);
+	plt.sca(ax1)
+	plt.ylabel('PCALipids eigenvectors for first trajectory')
+	plt.xlabel('PCALipids eigenvectors for second trajectory')
 	plt.show()
 
-
-if __name__ == '__main__':
-	args = sys.argv[1:]
-	if '-evec' in args:
-		main(args[args.index('-evec') + 1], args[args.index('-evec') + 2])
-	elif '-h' not in args and '-help' not in args:
-		print('Missing parameters, try -h for flags\n')
-	else:
-		print('-evec <two files with eigenvectors (example: -evec eigenvec1.xvg eigenvec2.xvg)>')
+# if __name__ == '__main__':
+# 	args = sys.argv[1:]
+# 	if '-evec' in args:
+# 		main(args[args.index('-evec') + 1], args[args.index('-evec') + 2])
+# 	elif '-h' not in args and '-help' not in args:
+# 		print('Missing parameters, try -h for flags\n')
+# 	else:
+# 		print('-evec <two files with eigenvectors (example: -evec eigenvec1.xvg eigenvec2.xvg)>')
