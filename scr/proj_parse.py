9b44d962ee0684f56602fb8e0ede3ed5442a9849
Khalid <khalid.mustafin@gmail.com>
2018-08-02 14:49:35 +0300

    splitproj fix

--- scr/proj_parse.py
+++ scr/proj_parse.py
@@ -1,14 +1,15 @@
 def main(filename):
 	file = open(filename, 'r')
 	proj_n = 1
-	file_out = open('proj_1.xvg', 'w')
+	name = filename.split('.')[0]
+	file_out = open('%s_1.xvg' % name, 'w')
 	for line in file:
 		if line.find('@') != -1:
 			continue
 		elif line.find('&') != -1:
 			file_out.close()
 			proj_n += 1
-			file_out = open('proj_%s.xvg' % proj_n, 'w')
+			file_out = open('%s_%s.xvg' % (name, proj_n), 'w')
 		else:
 			file_out.write(line.split()[1] + '\n')
 	file_out.close()
