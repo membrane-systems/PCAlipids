f352bc699e031b682b2d844f5c344389ca4251a8
Pavel Buslaev <pbuslaev@gmail.com>
2019-03-05 17:01:04 +0300

    Reworked conspace feature

--- scr/lipic.py
+++ scr/lipic.py
@@ -21,24 +21,11 @@ def lipic(traj_file, top_file, stride):
 
 
 def main(traj_file, top_file, stride, mot_out):
-	traj = lipic(traj_file, top_file, stride)
-	traj.save_pdb(mot_out)
-	print('Motion saved in file "%s"' % mot_out)
+	if not traj_file or not top_file:
+		print("Trajectory and topology have to be provided. \n\
+			Please run pcalipids.py conspace -h for help")
+	else:
+		traj = lipic(traj_file, top_file, stride)
+		traj.save_pdb(mot_out)
+		print('Motion saved in file "%s"' % mot_out)
 
-
-if __name__ == '__main__':
-	args = sys.argv[1:]
-	if '-stride' in args:
-		stride = int(args[args.index('-stride') + 1])
-	else:
-		stride = 10000
-	if '-om' in args:
-		mot_out = args[args.index('-om') + 1]
-	else:
-		mot_out = 'conform.pdb'
-	if '-f' in args and '-t' in args:
-		main(args[args.index('-f') + 1], args[args.index('-t') + 1], stride, mot_out)
-	elif '-h' not in args and '-help' not in args:
-		print('Missing parameters, try -h for flags\n')
-	else:
-		print('-f <trajectory file> (file format *.xtc)\n-t <topology file> (any file with topology)\n -stride <<positive integer; step of reading frames>\n -om <output file with conformations>')
