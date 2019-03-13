92004bd8bf9719c1b7a2c9eb0da5aa547894a9e8
Pavel Buslaev <pbuslaev@gmail.com>
2019-03-13 18:30:56 +0300

    con_traj reworked RAM and time usage

--- scr/con_traj.py
+++ scr/con_traj.py
@@ -3,29 +3,20 @@ import numpy as np
 from multiprocessing import Pool
 import sys
 import os
+import time
+import gc
 #from numba import jit
 
 
-def load_traj(traj_file, traj_top, lipid_resname, stride, sf, ef = None, max_frames = None):
-	print('Loading trajectory..')
-	if sf == None and ef == None:
-		traj = md.load(traj_file, top = traj_top, stride = stride)
-	else:
-		traj = md.load(traj_file, top = traj_top, stride = stride)[sf:ef]
-	print('Removing solvent..')
-	if lipid_resname == None:
-		traj = traj.remove_solvent()
-		traj = traj.atom_slice(traj.topology.select('not water and not type W H Hs WT4 NaW KW CLW MgW'))
-	else:
-		traj = traj.remove_solvent()
-		traj = traj.atom_slice(traj.topology.select('not water and not type W H Hs WT4 NaW KW CLW MgW and resname %s' % lipid_resname))
-	print(traj)
-	# # table, bonds  = traj.topology.to_dataframe()
-	# table.loc[:, ('chainID')] = 0
-	# table.loc[:, ('segmentID')] = 'A'
-	# topology = md.Topology.from_dataframe(table, bonds)
-	# new_traj = md.Trajectory(traj.xyz.astype(np.float64), topology = topology)
-	# traj = 0
+def load_traj(traj_file, traj_top, lipid_resname, stride, sf, ef, max_frames = None):
+	# create topology
+	topol = md.load(traj_top).topology
+	# select atoms for loading
+	ailist = topol.select('not water and not type W H Hs WT4 NaW KW CLW MgW and resname %s' \
+			% lipid_resname)
+	# load trajectory
+	traj = md.load(traj_file, top = traj_top, stride = stride, \
+		atom_indices = ailist)[sf:ef]
 	return traj
 
 
@@ -34,91 +25,66 @@ def concat(traj, lipid):
 
 
 def average_structure(traj):
-	avg_xyz = traj.xyz.astype(np.float64)
-	avg_xyz = avg_xyz.mean(axis = 0, dtype=np.float64)
+	avg_xyz = traj.xyz.astype(np.float64).mean(axis = 0, dtype=np.float64)
 	avg_traj = md.Trajectory([avg_xyz], traj.top)
 	return avg_traj
 
 
 #@jit
-def main(file_1, file_2, stride, sf, ef, out_traj, out_top, file_3 = None, lipid_resname = None):
+def main(file_1, file_2, stride, sf, ef, \
+	out_traj, out_top, file_3, lipid_resname):
+	
+	if not file_1 or not file_2 or not lipid_resname:
+		print("Trajectory and topology files have to be provided.\n\
+Name of residue of interest have to be indicated.\n\
+Run pcalipids.py concat -h for help")
+		return 0
+
+
+	# load trajectory
 	PATH = os.getcwd() + '/'
-	traj = load_traj(PATH + file_1, PATH + file_2, lipid_resname = lipid_resname, stride = stride, sf = sf, ef = ef)
-	N = traj[0].n_residues
-	#lipids = [(traj,i)  for i in range(N)]
-	#with Pool(2) as p:
-	#	trajs_ = p.starmap(concat, lipids)
-	trajs_ = []
-	i = 0
-	j = 0
-	while j < N:
-		# print(i)
-		try:
-			trajs_.append(concat(traj, i))	
-			i+= 1
-			j+= 1
-		except IndexError:
-			i += 1
-	traj = trajs_[0]
-	for i in range(1, N):
-		# print(i)
-		traj = traj.join(trajs_[i])
-		trajs_[i] = 0
-	if file_3 != None:
+	traj = load_traj(PATH + file_1, PATH + file_2, \
+		lipid_resname = lipid_resname, stride = stride, sf = sf, ef = ef)
+	print("Loaded trajectory "+file_1)
+
+	# get number of frames, lipids and atoms
+	nframes = traj.n_frames
+	nlip    = len(list(traj.topology.residues))
+	na      = traj.xyz.shape[1]//nlip
+	
+	# create concatenated trajectory
+	trajxyz = traj.xyz
+	trajxyz = trajxyz.reshape((nframes,nlip,na,3))
+	trajxyz = np.swapaxes(trajxyz,0,1)
+	trajxyz = trajxyz.reshape((nframes*nlip,na,3))
+
+	traj = md.Trajectory(trajxyz,traj[0].atom_slice(range(na)).topology)
+	
+	print("Created concatenated trajectory")
+
+	# aling frames in the concatenated trajectory
+	if file_3:
+		# if reference structure is provided:
+		# align all frames to the reference structure
 		ref_traj = md.load(file_3)
-		print('The trajectory was aligned, reference = %s.' % file_3)
+		traj.superpose(ref_traj, parallel = True)
 	else:
+		# if reference frame is not provided
+		# align all frames to the first frame of the first lipid
+		# then align all frames to the average structure of the aligned traj
 		ref_traj = traj[0]
-		print('The trajectory was aligned, reference = first frame.')
-	traj = traj.superpose(ref_traj, parallel = True)
-	if out_traj == None:
-		traj.save(PATH + 'concatenated.xtc')
-		print('Concatenated trajectory was created, saved in "concatenated.xtc"')
-	else:
-		traj.save(PATH + out_traj)
-		print('Concatenated trajectory was created, saved in "%s"' % out_traj)
-	avg_str = average_structure(traj)
-	traj = traj.superpose(avg_str, parallel = True)
-	avg_str = average_structure(traj)
-	if out_top == None:
-		avg_str.save('average.pdb')
-		print('Average structure saved in "average.pdb"')
-	else:
-		avg_str.save(out_top)
-		print('Average structure saved in "%s"' % out_top)
+		traj.superpose(ref_traj, parallel = True)
+		avg_str = average_structure(traj)
+		traj.superpose(ref_traj, parallel = True)
+	print('The trajectory was aligned, reference = %s.' % file_3) if file_3 else \
+	print('The trajectory was aligned to the average structure')
+	
+	# save concatenated trajectory
+	trajConc.save(PATH + out_traj)
+	print('Concatenated trajectory was created, saved in %s' % out_traj)
+	
+	# save average structure
+	avg_str = average_structure(trajConc)
+	avg_str.save(out_top)
+	print('Average structure saved in "%s"' % out_top)
 	return
-
-
-# if __name__ == '__main__':
-# 	args = sys.argv[1:]
-# 	if '-stride' in args:
-# 		stride = int(args[args.index('-stride') + 1])
-# 	else:
-# 		stride = None
-# 	if '-sf' in args:
-# 		sf = int(args[args.index('-sf') + 1])
-# 	else:
-# 		sf = None
-# 	if '-oc' in args:
-# 		out_traj = args[args.index('-oc') + 1]
-# 	else:
-# 		out_traj = None
-# 	if '-oa' in args:
-# 		out_top = args[args.index('-oa') + 1]
-# 	else:
-# 		out_top = None
-# 	if '-f' in args and '-t' in args and '-r' in args and '-l' in args:
-# 		main(args[args.index('-f') + 1], args[args.index('-t') + 1], args[args.index('-r') + 1], args[args.index('-l') + 1], stride = stride, sf = sf, out_traj = out_traj, out_top = out_top)
-# 	elif '-f' in args and '-t' in args and '-r' not in args:
-# 		print('No reference file supplied. The first frame of trajectory will be used for alignment.')
-# 		main(args[args.index('-f') + 1], args[args.index('-t') + 1], stride = stride, sf = sf, out_traj = out_traj, out_top = out_top)
-# 	elif '-f' in args and '-t' in args and '-r' in args and '-l' not in args:
-# 		main(args[args.index('-f') + 1], args[args.index('-t') + 1], args[args.index('-r') + 1], stride = stride, sf = sf, out_traj = out_traj, out_top = out_top)
-# 	elif '-f' in args and '-t' in args and '-r' not in args and '-l' not in args:
-# 		main(args[args.index('-f') + 1], args[args.index('-t') + 1], stride = stride, sf = sf, out_traj = out_traj, out_top = out_top)
-# 	elif '-h' not in args:
-# 		print('Missing parameters, try -h for flags\n')
-# 	else:
-# 		print('-f <trajectory file> (file format *.xtc, *trr)\n-t <topology file> (any file with topology)\n-r <reference traj file> (any topology file). If not supplied, \
-# 			the first frame of trajectory will be used for alignment\n -l <lipid type> (example: -l DPPC)\n -stride <positive integer; step of reading frames>\n \
-# 			 -sf <time in ps; number to determine from which frame to read the trajectory>\n -oc <output trajectory file>\n -oa <output topology file>\n')
