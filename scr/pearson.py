92b2b585135864087fea0b44fb1694afe7f6e831
Didin Maxim <didin.maxim@yandex.ru>
2019-10-08 13:42:04 +0300

    changes in pearson

--- scr/pearson.py
+++ scr/pearson.py
@@ -4,5 +4,5 @@ import numpy as np
 def main(filename_1, filename_2):
 	pearson_value = np.corrcoef(np.loadtxt(filename_1).flatten('F'), np.loadtxt(filename_2).flatten('F'))[0][1]
 	print(pearson_value)
-	np.savetxt('pearson.dat', np.array([pearson_value]))
-	print('Value saved in "pearson.dat"')
+	np.savetxt('pearson.data', np.array([pearson_value]))
+	print('Value saved in "pearson.data"')
