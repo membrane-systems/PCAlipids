23cb2eb9124a7de48e9e033663d5312cc8f372d6
Pavel Buslaev <pbuslaev@gmail.com>
2019-11-25 15:46:09 +0300

    depricated old stuff in project.py

--- scr/project.py
+++ scr/project.py
@@ -23,39 +23,6 @@ def load_evecs(evecs):
 	eigenvecs = np.array(eigenvecs, dtype = np.float64)
 	return eigenvecs
 
-
-def get_proj(traj, first_PC, last_PC, aver, evecs, filename):
-	ref_aver_str = md.load(aver)
-	traj = traj.superpose(ref_aver_str)
-	mean_vec = ref_aver_str.xyz.astype(np.float64)
-	x_std = traj.xyz.astype(np.float64).reshape(traj.n_frames,traj.n_atoms*3).T
-	x_std = x_std - np.array([mean_vec.reshape(traj.n_atoms*3, )]).T
-	eigenvecs = load_evecs(evecs)
-	if first_PC == None and last_PC == None:
-		first_PC = 1
-		last_PC = len(eigenvecs)
-	elif first_PC == None:
-		first_PC = 1
-		last_PC = int(last_PC)
-	elif last_PC == None:
-		first_PC = int(first_PC)
-		last_PC = len(eigenvecs)
-	else:
-		first_PC = int(first_PC)
-		last_PC = int(last_PC)
-	files = []
-	for i in range(first_PC,last_PC + 1):
-		file = open(filename[:filename.rfind('.')]+'_'+str(i)+filename[filename.rfind('.'):],'w')
-		file.write('@    title "Projection %s"\n' % (i))
-		files.append(file)
-	for i in range(first_PC - 1, last_PC):
-		proj = (x_std).T.dot(eigenvecs[i])
-		files[i].write(''.join(('     ' + str(proj[j]) + '\n') for j in range(len(proj))))
-	for i in range(len(files)):
-		files[i].write('&\n')
-		files[i].close
-	print('Wrote %s projections in "%s".' % (len(files), files[0].name[:files[0].name.rfind('_')] + "*"))
-
 def get_proj_mem(traj, top, first_PC, last_PC, aver, evecs, filename):
 	print("Projections are calculated while saving memory, it may take some time.")
 	
@@ -63,9 +30,6 @@ def get_proj_mem(traj, top, first_PC, last_PC, aver, evecs, filename):
 	na = ref_aver_str.n_atoms
 	eigenvecs = load_evecs(evecs)
 	mean_vec = ref_aver_str.xyz.astype(np.float64).reshape(1, ref_aver_str.n_atoms * 3)
-	
-	first_PC = first_PC
-	last_PC = last_PC
 	
 	files = []
 	
