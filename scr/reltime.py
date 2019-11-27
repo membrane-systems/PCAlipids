8984c9eb11f7455d12e525e58895229c0fd600bd
Pavel Buslaev <pbuslaev@gmail.com>
2019-11-27 12:37:50 +0300

    Reltime ad hoc

--- scr/reltime.py
+++ scr/reltime.py
@@ -7,12 +7,14 @@ def get_data_from_file(file_name):
 	file_ = open(file_name, 'r')
 	lines = file_.readlines()
 	file_.close()
+	n = 0
 	for i in range(2, len(lines)):
+		n += 1
 		if lines[i].find('*') == -1 and lines[i] != '\n':
 			data.append(float(lines[i].split()[1]))
 		else:
 			break
-	return data[:162]
+	return data[:n]
 
 
 def get_data_from_file_1(file_name):
@@ -20,19 +22,25 @@ def get_data_from_file_1(file_name):
 	file_ = open(file_name, 'r')
 	lines = file_.readlines()[1:-1]
 	file_.close()
+	n = 0
 	for line in lines:
+		n += 1
 		data.append(float(line.split()[1]))
-	return data[:162]
+	return data[:n]
 
 
 
 def f(data_1, data_2, eigvals,outF):
 	beta = 0.
-	for eigval in eigvals:
+	
+	m = min(len(data_1),len(data_2),len(eigvals))
+	if (m < len(eigvals)):
+		print("It is recommended to calculate the relative timescales for all projections")
+	for eigval in eigvals[:m]:
 		beta += eigval
-		beta = 1. / beta
-		R = 0.
-	for i in range(162):
+	beta = 1. / beta
+	R = 0.
+	for i in range(m):
 		R += beta * eigvals[i] * math.log(data_1[i]/data_2[i])
 	file = open(outF, 'w')
 	file.write(str(math.exp(R)) + ' ' + str(1 / math.exp(R)))
